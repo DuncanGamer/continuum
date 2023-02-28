f3dc77f0c5a4dbfce4ef52eb35a8c9ffb4fc17a8
Daniel G <dgimenez123@gmail.com>
2023-02-28 11:30:20 +0100

    mejorando

---
+++ src/components/ShareProduct.js
@@ -0,0 +1,28 @@
+import { BsFacebook, BsTwitter, BsWhatsapp } from "react-icons/bs"
+
+import ShareItem from "./ShareItem"
+
+const ShareProduct = ({ id }) => {
+  const URL = `https://products/${id}`
+  return (
+    <div className="z-0">
+      <p className="ms:text-xs md:text-lg text-white font-semibold mb-2">Share this concert</p>
+      <div className="flex gap-2">
+        <ShareItem
+          icon={BsWhatsapp}
+          url={`https://api.whatsapp.com/send?text=${URL}`}
+        />
+        <ShareItem
+          icon={BsFacebook}
+          url={`https://www.facebook.com/sharer/sharer.php?u=${URL}`}
+        />
+        <ShareItem
+          icon={BsTwitter}
+          url={`https://twitter.com/intent/tweet?text=WOW!!!&url=${URL}`}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ShareProduct
--- src/pages/Concertunic.js
+++ src/pages/Concertunic.js
@@ -1,95 +1,77 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { DataEvento,unicoevento,Simagen } from '../components/functions/AxiosCalls'
-import { useEffect, useState } from 'react'
-import Baner from '../components/Baner'
-import '../image.css'
-
-
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import {
+  DataEvento,
+  unicoevento,
+  Simagen,
+} from "../components/functions/AxiosCalls";
+import { useEffect, useState } from "react";
+import Baner from "../components/Baner";
+import "../image.css";
+import ShareProduct from "../components/ShareProduct";
 
 const Concertunic = () => {
-
-
-
-  const params = useParams()
-  const [info, Setinfo] = useState([])
-  const [imagen, Setimagen] = useState([])
-  const [date, Setdate] = useState([])
-
+  const params = useParams();
+  const [info, Setinfo] = useState([]);
+  const [imagen, Setimagen] = useState([]);
+  const [date, Setdate] = useState([]);
 
   useEffect(() => {
-
-    unicoevento(params.id, Setinfo)
-    Simagen(params.id, Setimagen)
-    DataEvento(params.id, Setdate)
-
-  }, [])
-
-
-
+    unicoevento(params.id, Setinfo);
+    Simagen(params.id, Setimagen);
+    DataEvento(params.id, Setdate);
+  }, []);
 
   return (
-
-
     <>
-    <div key = {info}>
-      <div className="pt-16 bg-black sm:pt-36">
-        <div className=" container m-auto">
-          <div id="carouselDarkVariant" className="carousel slide carousel-fade carousel-dark " data-bs-ride="carousel">
+      <div key={info}>
+        <div className="pt-16 bg-black sm:pt-36">
+          <div className=" container m-auto">
+            <div
+              id="carouselDarkVariant"
+              className="carousel slide carousel-fade carousel-dark "
+              data-bs-ride="carousel"
+            >
+              <div className="z-1 carousel-inner  relative w-full overflow-hidden">
+                <div className="carousel-item active relative float-left w-full h-3/5">
+                  <img
+                    src={imagen}
+                    className="  md:mt-0 block w-full h-full "
+                    alt="Motorbike Smoke"
+                  />
 
-
-
-            <div className="z-1 carousel-inner  relative w-full overflow-hidden">
-
-              <div className="carousel-item active relative float-left w-full">
-                <img src={imagen} className="  block w-full" alt="Motorbike Smoke" />
-                
-                <div className='flex justify-center  absolute top-0 right-0 w-full h-full bg-gradient-to-t from-black to-transparent'>
-                  <h3 className=' mt-auto mb-10 z-0 text-white text-7xl py-10'>{info.name}</h3>
+                  <div className="flex justify-center  absolute top-0 right-0 w-full h-full bg-gradient-to-t from-black to-transparent">
+                    <h3 className=" mt-auto mb-20 z-0 text-white ms:text-3xl md:text-6xl py-10">
+                      {info.name}
+                    </h3>
+                  </div>
+                  <div className="flex justify-end sm:mr-8 md:mr-20 ">
+                    <ShareProduct />
+                  </div>
                 </div>
               </div>
-    
             </div>
-
-
           </div>
-      
-        </div>
-      </div>
-
-      <div className='container m-auto h-96'>
-        <div>
-        
-          <p > Locale: {info.locale}</p>
-          <p > Date: {date.localDate}</p>
-          <p> Time: {date.localTime}</p>
-          <a href={info.url}> <p className=' underline decoration-2 '>  Buy it in ticketmaster.com</p></a>
-
         </div>
 
+        <div className=" bg-gradient-to-b from-black to-transparent w-full h-screen m-auto ">
+          <div className="container mx-auto text-white text-xl border-solid ">
+            
+            <p> Date: {date.localDate}</p>
+            <p> Time: {date.localTime}</p>
+            <p> Locale: {info.locale}</p>
 
-
-
+            <a href={info.url}>
+              <p className=" underline decoration-2 ">
+                Buy it in ticketmaster.com
+              </p>
+            </a>
+          </div>
+        </div>
+        <Baner />
       </div>
-      <Baner/>
-
-</div>
-
-
-
-
-
     </>
+  );
+};
 
-
-
-
-
-
-
-  )
-
-}
-
-export default Concertunic
+export default Concertunic;
